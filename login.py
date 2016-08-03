4c9b617104c188c3f62b7bab92d7e32fe14e2fe9
Dima <dmytroemail@yandex.ua>
2016-08-03 20:20:43 +0300

    Test suite for valid and invalid login

--- login.py
+++ login.py
@@ -4,7 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.keys import Keys
-from user_info import user_name, user_password, full_user_name
+from user_info import user_name, user_password, full_user_name, wrong_password
+from other_information import end_of_user_url, error_message
 
 class HomePage(object):
     '''Home page with login fields'''
@@ -32,7 +33,10 @@ class UserPage(object):
         self.driver = driver
 
     def print_status(self):
-        print ('User page is succesfully loaded')
+        if end_of_user_url in self.driver.current_url:
+            print ('User page is succesfully loaded')
+        else:
+            print ('User page is not loaded. User stayes at login page')
 
     def get_logined_user_name(self):
 
@@ -41,8 +45,13 @@ class UserPage(object):
         print ('User loginned as', self.logined_user_name)
 
         return self.logined_user_name
-
-
+    
+    def get_error_message(self):
+        
+        view_login_error_message = WebDriverWait(self.driver, 10).until(
+                        ec.presence_of_element_located((By.CLASS_NAME, "field-validation-error")))      
+        text_to_return = view_login_error_message.text
+        return text_to_return
 
 class TestLogin(unittest.TestCase):
 
@@ -53,16 +62,24 @@ class TestLogin(unittest.TestCase):
         self.driver.implicitly_wait(4)
 
     def tearDown(self):
-        print ("END OF THE TEST")
+        #print ("END OF THE TEST")
         self.driver.close()
 
-
+    @unittest.skip('skips test of test_valid_login')
     def test_valid_login(self):
         '''User tries login using valid name and password'''             
         home_page = HomePage(self.driver)
         user_home_page = home_page.login(user_name, user_password)
         user_home_page.print_status()
         assert full_user_name in user_home_page.get_logined_user_name()
+
+    def test_invalid_login(self):
+        '''User tries login using invalid password'''             
+        home_page = HomePage(self.driver)
+        user_home_page = home_page.login(user_name, wrong_password)
+        user_home_page.print_status()
+        assert error_message in user_home_page.get_error_message()
+        print (user_home_page.get_error_message())
 
 if __name__ == '__main__':
     unittest.main()
---
+++ other_information.py
@@ -0,0 +1,2 @@
+end_of_user_url = 'UserCards'
+error_message = 'Неправильный E-mail или пароль'
